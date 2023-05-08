39f2e169e68bd65d41536409b73153ec8c037902
Amir Hossein Moayedi <amir.h.moayedi@gmail.com>
2023-05-08 18:54:00 +0330

    feat(config): add database config
    add databse dsn config and log config

--- internall/config/config.go
+++ internall/config/config.go
@@ -2,15 +2,17 @@ package config
 
 import (
 	"fmt"
-	"github.com/amirhosseinmoayedi/Project-template/internall/log"
 	"github.com/spf13/viper"
+	gLogger "gorm.io/gorm/logger"
+	"log"
 )
 
 var Configs Config
 
 type Config struct {
-	Logger LoggerConfig `mapstructure:"log"`
-	Server ServerConfig `mapstructure:"server"`
+	Logger   LoggerConfig   `mapstructure:"log"`
+	Server   ServerConfig   `mapstructure:"server"`
+	DataBase DataBaseConfig `mapstructure:"database"`
 }
 
 type LoggerConfig struct {
@@ -26,21 +28,62 @@ func (sc ServerConfig) Address() string {
 	return fmt.Sprintf("%v:%v", sc.Host, sc.Port)
 }
 
+type DataBaseConfig struct {
+	Host     string `mapstructure:"host"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Name     string `mapstructure:"name"`
+	Port     string `mapstructure:"port"`
+	SSLMode  string `mapstructure:"ssl_mode"`
+	Timezone string `mapstructure:"timezone"`
+	Logger   struct {
+		SlowThreshold             int    `mapstructure:"slow_threshold"`
+		LogLevel                  string `mapstructure:"log_level"`
+		IgnoreRecordNotFoundError bool   `mapstructure:"ignore_record_not_found_error"`
+		ParameterizedQueries      bool   `mapstructure:"parameterized_queries"`
+	} `mapstructure:"logger"`
+}
+
+func (dbc DataBaseConfig) GetLogLevel() gLogger.LogLevel {
+	switch dbc.Logger.LogLevel {
+	case "silent":
+		return gLogger.Silent
+	case "error":
+		return gLogger.Error
+	case "warn":
+		return gLogger.Warn
+	default:
+		return gLogger.Info
+	}
+}
+
+func (dbc DataBaseConfig) DSN() string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		dbc.Host, dbc.User, dbc.Password, dbc.Name, dbc.Port, dbc.SSLMode, dbc.Timezone,
+	)
+}
+
 func InitConfig(configFile string) {
 	v := viper.New()
 
 	v.SetDefault("log", map[string]string{"level": "debug"})
 	v.SetDefault("server", map[string]string{"port": "8080", "host": "127.0.0.1"})
+	v.SetDefault("database", map[string]any{
+		"host": "localhost", "user": "postgres", "password": "postgres",
+		"db_name": "user", "port": 5432, "ssl_mode": "disable", "timezone": "utc"},
+	)
 
 	v.SetConfigFile(configFile)
 
+	log.Printf("Parsing Config File")
 	err := v.ReadInConfig()
 	if err != nil {
-		log.Logger.WithError(err).Fatal("error reading config file")
+		log.Fatalf("error reading config file, err:%s", err)
 	}
 
 	err = v.Unmarshal(&Configs)
 	if err != nil {
-		log.Logger.WithError(err).Warn("error parsing configs to Config struct")
+		log.Printf("error parsing configs to Config struct:%s", err)
 	}
 }
